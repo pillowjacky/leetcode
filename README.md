070640374b4410ffe2dc865e33977c86d88f441d
Jacky Chung <pillowjacky@gmail.com>
2022-05-30 00:42:21 +0800

    feat: 57. insert interval

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # leetcode
 
-| #   | Problem                                           | Difficulty |
-|-----|---------------------------------------------------|------------|
-| 1   | [Two Sum](https://leetcode.com/problems/two-sum/) | Easy       |
+| #   | Problem                                                           | Difficulty |
+| --- | ----------------------------------------------------------------- | ---------- |
+| 1   | [Two Sum](https://leetcode.com/problems/two-sum/)                 | Easy       |
+| 57  | [Insert Interval](https://leetcode.com/problems/insert-interval/) | Medium     |
---
+++ _57_insert_interval/Solution.java
@@ -0,0 +1,25 @@
+package _57_insert_interval;
+
+import java.util.LinkedList;
+
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        var result = new LinkedList<>();
+
+        int i = 0;
+        while (i < intervals.length && intervals[i][1] < newInterval[0]) {
+            result.add(intervals[i++]);
+        }
+        while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
+            newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
+            i++;
+        }
+        result.add(newInterval);
+        while (i < intervals.length) {
+            result.add(intervals[i++]);
+        }
+
+        return result.toArray(new int[0][0]);
+    }
+}
