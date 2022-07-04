20cf16dc8517cbdda1926ed8b5411049943dd799
Jacky Chung <pillowjacky@gmail.com>
2022-07-04 23:28:02 +0800

    feat: 15. 3sum

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 | --- | ------------------------------------------------------------------------------------------------------------------------------- | ---------- |
 | 1   | [Two Sum](https://leetcode.com/problems/two-sum/)                                                                               | Easy       |
 | 3   | [Longest Substring Without Repeating Characters](https://leetcode.com/problems/longest-substring-without-repeating-characters/) | Medium     |
+| 15  | [3Sum](https://leetcode.com/problems/3sum/)                                                                                     | Medium     |
 | 57  | [Insert Interval](https://leetcode.com/problems/insert-interval/)                                                               | Medium     |
 | 542 | [01 Matrix](https://leetcode.com/problems/01-matrix/)                                                                           | Medium     |
 | 973 | [K Closest Points to Origin](https://leetcode.com/problems/k-closest-points-to-origin/)                                         | Medium     |
---
+++ _15_3Sum/OptimizationTwoPointers.java
@@ -0,0 +1,44 @@
+package _15_3Sum;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class OptimizationTwoPointers {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+
+        List<List<Integer>> out = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    out.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (left < right) {
+                        left++;
+                        if (nums[left] != nums[left - 1]) {
+                            break;
+                        }
+                    }
+                    while (left < right) {
+                        right--;
+                        if (nums[right] != nums[right + 1]) {
+                            break;
+                        }
+                    }
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return out;
+    }
+}
