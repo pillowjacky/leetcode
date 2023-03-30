8660432179e9314be058c37cf9c1e4a91209e309
Jacky CHUNG <pillowjacky@gmail.com>
2023-03-30 10:24:29 +0800

    704. Binary Search

---
+++ java/0704-binary-search.java
@@ -0,0 +1,28 @@
+package java;
+
+/**
+ * <pre>
+ * 
+ * 704. Binary Search
+ * https://leetcode.com/problems/binary-search/
+ * 
+ * </pre>
+ */
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
